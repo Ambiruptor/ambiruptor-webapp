fc5de13132a5f19887fd1a01b0f663345a5bcbc0
Simon Mauras <simon.mauras@ens-lyon.fr>
2016-02-17 11:38:42 +0100

    Dummy disambiguator

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/disambiguate.json', methods=['POST'])
+def disambiguate():
+    print("Received : %s" % request.form["text"])
+    dummy_data = """[\
+{"begin" : 10, "end" : 15, "url" : "https://en.wikipedia.org/wiki/Bar"},
+{"begin" : 18, "end" : 30, "url" : "https://en.wikipedia.org/wiki/Plant"}]"""
+    return dummy_data
 
 if __name__ == '__main__':
     app.run()
