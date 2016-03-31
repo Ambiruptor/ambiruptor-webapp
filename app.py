c14024cf8c321b6342e7c036e1ebe87f7df0eaf9
Eugenia <yarutoua@gmail.com>
2016-03-31 18:41:54 +0200

    Modified: server precesses the corrections (dummy way)

--- app.py
+++ app.py
@@ -2,17 +2,61 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/disambiguate.json', methods=['POST'])
 def disambiguate():
     print("Received : %s" % request.form["text"])
     dummy_data = """[\
-{"begin" : 10, "end" : 15, "url" : "https://en.wikipedia.org/wiki/Bar"},
-{"begin" : 18, "end" : 30, "url" : "https://en.wikipedia.org/wiki/Plant"}]"""
+        {
+            "begin" : 10,
+            "end" : 15,
+            "sense" : "Bar (place)",
+            "url" : "https://en.wikipedia.org/wiki/Bar"
+        },
+        {
+            "begin" : 18,
+            "end" : 30,
+            "sense" : "Plant (green)",
+            "url" : "https://en.wikipedia.org/wiki/Plant"
+        }
+    ]"""
     return dummy_data
+
+
+@app.route('/check-disambiguate.json', methods=['POST'])
+def check_disambigation():
+    print("Received : %s" % request.form["text"])
+    dummy_data = """[\
+        {
+            "begin" : 10,
+            "end" : 15,
+            "sense" : "Bar (place)",
+            "all_senses" : ["Bar (place)", "Bar (fish)", "Bar (chocolate)"],
+            "url" : "https://en.wikipedia.org/wiki/Bar"
+        },
+        {
+            "begin" : 18,
+            "end" : 30,
+            "sense" : "Plant (green)",
+            "all_senses" : ["Plant (green)", "Plant (factory)", "Plant (person)"],
+            "url" : "https://en.wikipedia.org/wiki/Plant"
+        }
+    ]"""
+    return dummy_data
+
+
+@app.route('/submit-corrections.json', methods=['POST'])
+def submit_corrections():
+    print("Received corrections to text:\n %s\n" % request.form["text"])
+    print("Disambiguation: %s" % request.form["disambiguation"])
+    print("Correct senses indices: %s" % request.form["sense_indices"])
+    return "Success!"
+    # TODO think how to save them nicely
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
